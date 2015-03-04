05c12aa08df1d12bcdc1fadb84198af3cca0d435
jorge.franco.garro@gmail.com <paloma2302>
2015-03-04 17:17:18 +0100

    Primera subida

---
+++ Alumnos/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Alumnos.Models;
+
+namespace Alumnos.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db = new CursosEntities())
+            {
+                return View(db.Alumnos.ToList());
+            }
+
+        }
+
+
+        public ActionResult Alta()
+        {
+
+            return View(new Models.Alumnos());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Models.Alumnos model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new CursosEntities())
+                {
+                    db.Alumnos.Add(model);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+
+
+        }
+
+
+
+
+
+    }
+}
